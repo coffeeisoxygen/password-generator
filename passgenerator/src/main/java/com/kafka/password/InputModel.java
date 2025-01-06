7f2036f0629e2de6b628a5e76e3d7bf53f50ab21
Hasan Maki <42813480+nekadcoding@users.noreply.github.com>
2025-01-06 18:22:48 +0700

    Add InputModel class for password management and validation

---
+++ passgenerator/src/main/java/com/kafka/password/InputModel.java
@@ -0,0 +1,37 @@
+package com.kafka.password;
+
+public class InputModel {
+
+    private String passwordText;
+    private int passwordLength;
+
+    //constructor
+    public InputModel(String passwordText, int passwordLength) {
+        this.passwordText = passwordText;
+        this.passwordLength = passwordLength;
+    }
+
+    // validation of not null
+    public boolean isValid() {
+        return passwordText != null && passwordLength > 0;
+    }
+
+    //getters
+    public String getPasswordText() {
+        return passwordText;
+    }
+
+    public int getPasswordLength() {
+        return passwordLength;
+    }
+
+    //setters
+    public void setPasswordText(String passwordText) {
+        this.passwordText = passwordText;
+    }
+
+    public void setPasswordLength(int passwordLength) {
+        this.passwordLength = passwordLength;
+    }
+
+}

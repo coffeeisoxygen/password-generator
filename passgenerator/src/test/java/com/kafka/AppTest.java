c32fcee39679772b46bc1f3786f0044bec20a1a5
Hasan Maki <42813480+nekadcoding@users.noreply.github.com>
2025-01-06 15:36:47 +0700

    Update .gitignore, fix Maven configuration, and refactor App and AppTest classes

--- passgenerator/src/test/java/com/kafka/AppTest.java
+++ passgenerator/src/test/java/com/kafka/AppTest.java
@@ -1,19 +1,14 @@
 package com.kafka;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+/** Unit test for simple App. */
+public class AppTest {
+  /** Rigorous Test :-) */
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
 }
